6527fbb0848b359ec2865c3830804ec2e9753e4b
ioosterl <14086799+ioosterl@users.noreply.github.com>
2021-01-17 18:32:31 +0100

    Playing around with Set.of

---
+++ src/test/java/be/ioosterl/explore/java9/SetFactoryMethods.java
@@ -0,0 +1,48 @@
+package be.ioosterl.explore.java9;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class SetFactoryMethods {
+
+  @Test
+  void setOf() {
+    Set<Integer> set = Set.of(1, 2, 3);
+    assertThat(set).hasSize(3);
+  }
+
+  @Test
+  void setOfWithDuplicates() {
+    assertThatExceptionOfType(IllegalArgumentException.class)
+        .isThrownBy(() -> Set.of(1, 2, 2));
+  }
+
+  @Test
+  void setOfThenAdd() {
+    Set<Integer> mySet = Set.of(1,2);
+    assertThatExceptionOfType(UnsupportedOperationException.class)
+        .isThrownBy(() -> mySet.add(3));
+  }
+
+  @Test
+  void setOfVarArgsOrArray() {
+    Set<Integer> fromArray = Set.of(new Integer[] {1, 2, 3});
+    Set<Integer> directly = Set.of(1,2,3);
+    assertThat(fromArray).isEqualTo(directly);
+    assertThat(fromArray).isNotSameAs(directly);
+  }
+
+  @Test
+  void setOfWrapperObjects() {
+    Set<Wrap<Integer>> set = Set.of(Wrap.around(1), Wrap.around(2));
+    assertThat(set).hasSize(2);
+    assertThat(set).extracting("value").containsOnly(1,2);
+
+    set.forEach(w -> w.reWrap(w.unWrap()+1));
+    assertThat(set).extracting("value").containsOnly(2,3);
+  }
+}
